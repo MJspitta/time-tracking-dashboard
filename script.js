875181bf7844df9dc442a4e2a3ef59c9d51e34e6
Madu Jang <madu.jang93@gmail.com>
2025-07-30 00:47:00 +0100

    fetching tracking data

---
+++ script.js
@@ -0,0 +1,58 @@
+const getTrackingData = async () => {
+  try {
+    const res = await fetch('./data.json');
+    const data = await res.json();
+
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const trackingSection = document.querySelector('.tracking-section');
+
+const displayTrackingData = async (period) => {
+  const cardsData = await getTrackingData();
+  trackingSection.innerHTML = "";
+
+
+  cardsData.map((card) => {
+    const currDir = card.timeframes[period].current;
+    const prevDir = card.timeframes[period].previous;
+
+    trackingSection.innerHTML += `
+    <div class="timetrack-card ${card.title.toLowerCase().split(" ").join("-")}">
+      <div class="card-content">
+        <div class="card-title">
+          <h2>${card.title}</h2>
+          <a href="#">
+            <img src="./images/icon-ellipsis.svg" alt="options icon" />
+          </a>
+        </div>
+        <div class="card-time">
+          <p class="current-time">${currDir}hrs</p>
+          <p class="previous-time">Last Week - ${prevDir}hrs</p>
+        </div>
+      </div>
+    </div>
+    `;
+  });
+}
+
+const trackingPeriods = document.querySelectorAll('input[name="track-periods"]');
+
+for (const p of trackingPeriods) {
+  if (p.checked) {
+    displayTrackingData(p.id);
+    break;
+  }
+}
+
+trackingPeriods.forEach((period) => {
+  period.addEventListener('change', () => {
+    displayTrackingData(period.id);
+  })
+});
+
